182245d55110cb5de018cd2b792b013297b859ad
thiru <thiru@axiomconsulting.in>
2025-08-21 20:04:50 +0530

    header and cta

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 function Contact() {
   return (
-    <section className="container text-center my-5 rounded text-white cta p-5 d-flex flex-column gap-2 align-items-center" id="contact">
+    <section className="container text-center my-5 rounded text-white cta p-md-5 p-1 d-flex flex-column gap-2 align-items-center" id="contact">
       <img src="https://cdn.prod.website-files.com/686f231d23a1ec789d436e46/686f2b8cecf51144e81d6391_Vector.png" className="cta-image" alt="" />
       <h2 className="text-center">Let's Build Something Great Together</h2>
       <p className="text-center">I’m currently seeking new opportunities to contribute to impactful projects, solve real-world problems, and help turn ideas into scalable web solutions. I’m ready to bring your vision to life with clean, efficient, and modern code.</p>
