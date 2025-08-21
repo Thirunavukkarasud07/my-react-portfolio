3c6da01f7a915e09ce856ee6fd45f02f2d3b2e01
thiru <thiru@axiomconsulting.in>
2025-08-21 19:47:42 +0530

    your commit message

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,15 @@
 function Header() {
   return (
-    <header className="d-flex justify-content-between px-5 py-3 border border-bottom shadow--bottom-sm z-1 bg-white sticky-header top-0 w-100">
-      <div className="d-md-block col-md-6 col-0 d-none"><h3 className="z-1">Thiru.D</h3></div>
+    <header className="z-1">
+     <div className="header d-flex justify-content-between z-1 py-2 px-4">
+       <div className="d-md-block col-md-6 col-0 d-none"><h3 className="z-1">Thiru.D</h3></div>
       <nav className="col-md-6 col-12 d-flex z-1 gap-md-5 mb-0 justify-content-md-end justify-content-around align-items-center">
         <a href="#about">About</a>
         <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
       </nav>
+     </div>
     </header>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,10 @@
 function Contact() {
   return (
-    <section className="container text-center my-5 border rounded bg-primary text-white p-5 d-flex flex-column gap-2 align-items-center" id="contact">
+    <section className="container text-center my-5 rounded text-white cta p-5 d-flex flex-column gap-2 align-items-center" id="contact">
+      <img src="https://cdn.prod.website-files.com/686f231d23a1ec789d436e46/686f2b8cecf51144e81d6391_Vector.png" className="cta-image" alt="" />
       <h2 className="text-center">Let's Build Something Great Together</h2>
       <p className="text-center">I’m currently seeking new opportunities to contribute to impactful projects, solve real-world problems, and help turn ideas into scalable web solutions. I’m ready to bring your vision to life with clean, efficient, and modern code.</p>
-      <a href="tel:09025407738" className="btn btn-light">Hire Me</a>
+      <a href="tel:09025407738" className="btn btn-light text-dark">Hire Me</a>
     </section>
   );
 }
